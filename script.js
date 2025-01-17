42a05e33e1080ad65a8b903f09ba386c27b086af
rifqah aulia <rifqahauliasalsabilla173@gmail.com>
2025-01-17 19:35:23 +0700

    proses

---
+++ script.js
@@ -0,0 +1,8 @@
+// navbar toggling
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
